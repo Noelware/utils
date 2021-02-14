b3dc83a2567328b13ff43454477487d501290f2c
Noel <august@augu.dev>
2021-02-14 01:16:09 -0700

    1.0.3

--- a.js
+++
@@ -1,8 +0,0 @@
-const utils = require('./build');
-
-const currDate = new Date();
-const yearFromNow = new Date(2022, currDate.getMonth(), currDate.getDate(), currDate.getHours(), currDate.getMinutes(), currDate.getSeconds(), currDate.getSeconds()).getTime();
-
-console.log(utils.humanize(yearFromNow - currDate.getTime()));
-console.log(utils.humanize(yearFromNow - currDate.getTime(), true));
-console.log(yearFromNow - currDate.getTime());
